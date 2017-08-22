bfd89839cf57968e45abef112fd890b142fa5e8d
Anandaraju Coimbatore Sivaraju <ana.ceg92@gmail.com>
2017-08-22 00:40:31 -0400

    New components and axios GET call

--- albums/index.android.js
+++ albums/index.android.js
@@ -2,12 +2,16 @@
 
 // import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // create a component
 const App = () => (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 // Render it to the device
--- albums/src/components/Header.js
+++ albums/src/components/Header.js
@@ -18,7 +18,7 @@ const styles = {
 		paddingTop: 15,
 		shadowColor: '#000',
 		shadowOffset: { width: 0, height: 2 },
-		shadowOpacity: 0.1,
+		shadowOpacity: 0.2,
 		alignItems: 'center',
 		justifyContent: 'center',
 		elevation: 2,
